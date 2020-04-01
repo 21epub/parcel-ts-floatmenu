ea3cfb7d20c564f58bdd5ffe2f031d06065da809
zhupengcheng <1071667708@qq.com>
2020-04-01 16:55:01 +0800

    feat:添加sync同步代码功能

--- src/ts/ContentMenu.tsx
+++ src/ts/ContentMenu.tsx
@@ -11,21 +11,23 @@ function ContextMenu(props: any) {
     console.log('复制')
   }
 
+
+
   // 菜单dom结构
   const menu = (
     <Menu>
-    <Menu.Item key='1' disabled>删除（禁止选中）</Menu.Item>
-    <Menu.Item onClick={()=>clickDom(123)}>复制</Menu.Item>
-    <Menu.Divider/>
-    <SubMenu title="二级菜单">
-      <Menu.Item>删除</Menu.Item>
-      <Menu.Item>复制</Menu.Item>
-    </SubMenu>
-    <SubMenu title="禁止选中" disabled>
-      <Menu.Item>5d menu item</Menu.Item>
-      <Menu.Item>6th menu item</Menu.Item>
-    </SubMenu>
-  </Menu>
+      <Menu.Item key='1' disabled>删除（禁止选中）</Menu.Item>
+      <Menu.Item onClick={()=>clickDom(123)}>复制</Menu.Item>
+      <Menu.Divider/>
+      <SubMenu title="设置">
+        <Menu.Item>删除</Menu.Item>
+        <Menu.Item>复制</Menu.Item>
+      </SubMenu>
+      <SubMenu title="关于我们">
+        <Menu.Item>官网</Menu.Item>
+        <Menu.Item>作品</Menu.Item>
+      </SubMenu>
+    </Menu>
   );
 
   return true &&
